fe5e476d783bc58c75b3068a370ec93293db9e4b
Ian-Almeida <ian.almeida@edu.univali.br>
2021-05-06 21:35:14 -0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Switch } from "react-router-dom";
+
 import ContratoLocacao from "./components/ContratoLocacao/ContratoLocacao";
+import React from "react";
 
 
 const App = () => {
   return (
     <div>
-        <ContratoLocacao />
+        <Switch>
+            <Route exact path="/contrato-generator/">
+                <ContratoLocacao />
+            </Route>
+        </Switch>
     </div>
   );
 }
